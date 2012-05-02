f795d40934dea02c605fc4bbfe35509be7aa8b5d
Tomer Paz <tomer.paz@gmail.com>
2012-05-02 18:57:03 +0300

    an 'all in one' example that will work with single C&P (hopefully) for developers to test cross-site requests using Django

--- cors/middleware.py
+++ cors/middleware.py
@@ -1,15 +1,24 @@
+# a 'one fits all' example good for testing (careful in production!)
+# refer to http://www.w3.org/TR/cors for the SPEC
+# and nice explanation at: https://developer.mozila.org/En/HTTP_Access_Control for more details
 from django.http import HttpResponse
 
 
 class AllowOriginMiddleware(object):
     def process_request(self, request):
         if request.method == 'OPTIONS':
-            return HttpResponse()
+            response = HttpResponse()
+            response['Access-Control-Allow-Credentials'] = 'true'
+            response['Access-Control-Allow-Methods']     = 'POST, GET, OPTIONS'
+            response['Access-Control-Allow-Headers']     = 'Authorization,DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type'
+            response['Access-Control-Max-Age']           = 1728000  # 20 days is enough, not?
+            return response
 
     def process_response(self, request, response):
         origin = request.META.get('HTTP_ORIGIN')
         if origin:
             response['Access-Control-Allow-Origin'] = origin
             response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-            response['Access-Control-Allow-Headers'] = 'Content-Type'
+            response['Access-Control-Allow-Credentials'] = 'true'
+            response['Access-Control-Allow-Headers'] = 'Authorization,DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type'
         return response
